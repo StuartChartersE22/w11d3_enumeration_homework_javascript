606ae2b7066fdcda24f3eb221159a1fee688fe14
StuartCharters <charters.stuart@gmail.com>
2018-08-01 15:25:20 +0100

    can find all and locations for a persons journies

--- specs/traveller_spec.js
+++ specs/traveller_spec.js
@@ -38,7 +38,7 @@ describe('Traveller', function() {
     assert.deepStrictEqual(traveller.getJourneyStartLocations(), actual);
   });
 
-  xit('should be able to get the journeys end locations', function() {
+  it('should be able to get the journeys end locations', function() {
     const actual = [
       journey1.endLocation,
       journey2.endLocation,
